5da4ff0a76a8905eb5c4e9ded50bf1f1d25ba759
André Neves <a22922@d1-08-09.dped.local>
2023-02-13 16:14:29 +0000

    Setters and Getters criados Javadoc generado

--- src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
@@ -1,5 +1,7 @@
 package psi_11_o.m17b_fichatrabalho5;
-
+    /**
+     * @author André Neves, a22922
+     */
 public class Ponto2D {
     private int x;
     private int y;
@@ -15,11 +17,39 @@ public class Ponto2D {
     }
     
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
     }
 
     @Override
     public String toString() {
-        return "Ponto2D[" + x + "," + y + "]";
-    }    
+        return "Ponto2D[" + getX() + "," + getY() + "]";
+    }        
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
 }
